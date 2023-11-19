bcf37f4e295550d60f04ab4573bb447f28294c59
Srikar Reddy <gsrikar2010@gmail.com>
2023-11-19 15:34:54 +0530

    Send Error Reason incase in case of Error Event Response

--- BankConnect/webhelper/FinBoxWebViewHandler.swift
+++ BankConnect/webhelper/FinBoxWebViewHandler.swift
@@ -22,9 +22,24 @@ class FinBoxWebViewHandler: NSObject, WKScriptMessageHandler {
         let eventResponse = try? JSONDecoder().decode(WebEventResponse.self, from: bodyData)
         debugPrint("Entity Id", eventResponse?.payload.entityId ?? "Empty Entity Id")
         
-        let payload = FinBoxPayload(message: eventResponse?.message, linkId: eventResponse?.payload.linkId, entityId: eventResponse?.payload.entityId, error_type: nil)
+        
+        if (eventResponse?.status == "error") {
+            // Update the callback with error reason
+            setCallbackPayload(message: eventResponse?.payload.reason, eventResponse: eventResponse)
+        } else {
+            // Update the callback with payload message
+            setCallbackPayload(message: eventResponse?.payload.message, eventResponse: eventResponse)
+        }
+        
+    }
+    
+    // Update the callback payload
+    func setCallbackPayload(message: String?, eventResponse: WebEventResponse?) {
+        // Generate the callback payload
+        let payload = FinBoxPayload(message: message, linkId: eventResponse?.payload.linkId, entityId: eventResponse?.payload.entityId, error_type: eventResponse?.payload.error_type)
         debugPrint("Callback Entity Id", payload.entityId ?? "Empty Entity Id")
 
     }
+    
 }
 
