cf2348c4d91f1c3457a03b96bb75fd74c148cfdf
Srikar Reddy <gsrikar2010@gmail.com>
2023-11-22 07:54:06 +0530

    Format SessionViewModel

--- BankConnect/webhelper/SessionViewModel.swift
+++ BankConnect/webhelper/SessionViewModel.swift
@@ -6,22 +6,22 @@
 import Foundation
 
 class SessionViewModel: ObservableObject {
-
+    
     /// API url to get the session url
     let urlStr = "https://portal.finbox.in/bank-connect/v1/session/"
     
     @Published var sessionUrl: String?
-
+    
     // Fetch the Session URL from the Session API
     func fetch() {
         // Get the request body
         let requestBody = self.getRequestBody()
-
+        
         guard let sessionBody = requestBody else {
             debugPrint("Request body is null")
             return
         }
-
+        
         let task = URLSession.shared.dataTask(with: Utils.postRequest(urlStr: urlStr, body: sessionBody)) { data, response, error in
             if let error = error {
                 self.handleClientError(error: error)
@@ -29,16 +29,16 @@ class SessionViewModel: ObservableObject {
             }
             
             guard let httpResponse = response as? HTTPURLResponse,
-                        (200...299).contains(httpResponse.statusCode) else {
-                        self.handleServerError(response: response)
-                        return
-                    }
-
+                  (200...299).contains(httpResponse.statusCode) else {
+                self.handleServerError(response: response)
+                return
+            }
+            
             guard let data = data else {
                 self.handleClientError(error: "Failed to receive Redirect Url")
                 return
             }
-
+            
             do {
                 // Convert the response to Session Url
                 let sessionResponse: SessionResponse = try JSONDecoder().decode(SessionResponse.self, from: data)
@@ -50,7 +50,7 @@ class SessionViewModel: ObservableObject {
             } catch {
                 self.handleClientError(error: error)
             }
-
+            
         }
         task.resume()
     }
@@ -58,11 +58,11 @@ class SessionViewModel: ObservableObject {
     func getSessionRequest() -> SessionRequest? {
         // Get a reference to user defaults
         let userPref = UserPreference()
-
+        
         guard let linkId = userPref.linkId else {
             return nil
         }
-
+        
         guard let apiKey = userPref.apiKey else {
             return nil
         }
@@ -95,5 +95,5 @@ class SessionViewModel: ObservableObject {
     func handleServerError(response: URLResponse?) {
         debugPrint("Response Error \(response as URLResponse?)")
     }
-
+    
 }
