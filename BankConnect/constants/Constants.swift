6e084db3fe56f30841db968057194a3bead7c0af
Ashutosh Jena <ashutosh.jena@finbox.in>
2024-02-01 20:11:08 +0530

    BANC-1632 Add Constants

---
+++ BankConnect/constants/Constants.swift
@@ -0,0 +1,29 @@
+//
+//  Constants.swift
+//  FinBoxBankConnect
+//
+//  Created by Ashutosh Jena on 12/12/23.
+//
+
+import Foundation
+
+// UserDefaults Keys
+let FINBOX_BANK_CONNECT_API_KEY = "finbox-bank-connect-api-key"
+let FINBOX_BANK_CONNECT_LINK_ID = "finbox-bank-connect-link-id"
+let FINBOX_BANK_CONNECT_FROM_DATE = "finbox-bank-connect-from-date"
+let FINBOX_BANK_CONNECT_TO_DATE = "finbox-bank-connect-to-date"
+let FINBOX_BANK_CONNECT_BANK_NAME = "finbox-bank-connect-bank-name"
+let FINBOX_BANK_CONNECT_REDIRECT_URL = "finbox-bank-connect-redirect-url"
+let FINBOX_BANK_CONNECT_LOGO_URL = "finbox-bank-connect-logo-url"
+let FINBOX_BANK_CONNECT_MODE = "finbox-bank-connect-mode"
+let FINBOX_BANK_CONNECT_USER_TOKEN = "finbox-bank-connect-user-token"
+
+// Callback Status
+let FINBOX_BC_EXIT = "exit"
+let FINBOX_BC_SUCCESS = "success"
+let FINBOX_BC_ERROR = "error"
+let FINBOX_BC_INFO = "info"
+
+// Base URL
+let FINBOX_BC_BASE_URL = "https://apis.bankconnect.finbox.in/bank-connect/"
+let FINBOX_BC_SESSION_END_POINT = "v1/session/"
