d40d00bf7537c624515ce8e264a0be17afcda8f6
Ashutosh Jena <ashutosh.jena@finbox.in>
2024-02-01 20:17:45 +0530

    SDK Ready

---
+++ CHANGELOG.md
@@ -0,0 +1,25 @@
+#  CHANGELOG
+
+
+## 0.3.0
+
+Add Multi Bank Session Flow
+
+## 0.2.1
+
+Make Callback Payload Codable
+
+
+## 0.2.0
+
+Return Callbacks when success and exit events are triggered
+
+
+## 0.1.3
+
+BankConnect version built for IOS 13.0
+
+
+## 0.1.0
+
+BankConnect version built for IOS 14.0
---
+++ build/BankConnect.framework-iphoneos.xcarchive/Products/Library/Frameworks/BankConnect.framework/Headers/BankConnect.h
@@ -0,0 +1,18 @@
+//
+//  BankConnect.h
+//  BankConnect
+//
+//  Created by Srikar on 12/04/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BankConnect.
+FOUNDATION_EXPORT double BankConnectVersionNumber;
+
+//! Project version string for BankConnect.
+FOUNDATION_EXPORT const unsigned char BankConnectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BankConnect/PublicHeader.h>
+
+
