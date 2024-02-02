348c17a08c0e03001ec89f5bd6e7755e9e0610c1
Ashutosh Jena <ashutosh.jena@finbox.in>
2024-02-02 16:45:24 +0530

    BANC-1643 Update View Handling in BankView

--- BankConnect/BankView.swift
+++ BankConnect/BankView.swift
@@ -17,29 +17,44 @@ public struct BankView: View {
     
     @ObservedObject var viewModel = SessionViewModel()
     
+    /// This property tracks if the viewmodel is fetching session
+    /// State marks this as mutable
+    @State var fetchStarted = false
+    
     public init(bankResult: @escaping (FinBoxPayload) -> Void) {
         self.bankResult = bankResult
     }
     
     public var body: some View {
         VStack {
-            if (viewModel.sessionUrl != nil) {
-                // Load the webpage
-                FinBoxWebView(urlStr: viewModel.sessionUrl,
-                              bankResult: bankResult)
-            } else {
-                if #available(iOS 14, *) {
-                    // Show progress
-                    ProgressView()
+            if (fetchStarted) {
+                // ViewModel is fetching the session
+                if (viewModel.sessionResult?.error == nil) {
+                    if (viewModel.sessionResult?.sessionURL != nil) {
+                        // Load the webpage
+                        FinBoxWebView(urlStr: viewModel.sessionResult?.sessionURL,
+                                      bankResult: bankResult)
+                        let _ = debugPrint("BankView: Loading WebView")
+                    } else {
+                        handleError(error: "Invalid Session Url")
+                    }
                 } else {
-                    // Progress View is not available
+                    handleError(error: viewModel.sessionResult?.error ?? "Unknown Error")
                 }
+            } else {
+                // First time the screen is loaded
+                ProgressView()
             }
-            
         }.onAppear(perform: {
             // Fetch the session url
             viewModel.fetchSession()
+            self.fetchStarted = true
         })
+    }
+    
+    func handleError(error: String) -> some View {
+        bankResult(FinBoxPayload(message: error, linkId: "", entityId: "", errorType: ""))
+        return Text("\(String(describing: error))")
     }
 }
 
