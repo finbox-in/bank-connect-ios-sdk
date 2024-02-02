ceb52e7cd1ed0360bfb45dc02207758c04dc394f
Ashutosh Jena <ashutosh.jena@finbox.in>
2024-02-02 16:46:13 +0530

    Update SessionViewModel with SessionResult

--- BankConnect/viewmodels/SessionViewModel.swift
+++ BankConnect/viewmodels/SessionViewModel.swift
@@ -10,14 +10,14 @@ import Foundation
 /// A view model responsible for managing session-related data.
 class SessionViewModel: ObservableObject {
     
-    @Published var sessionUrl: String?
+    @Published var sessionResult: SessionResult?
     
     func fetchSession() {
         APIService.instance.fetchSession { result in
-            debugPrint("Received Session in viewmodel")
+            debugPrint("Received Result in viewmodel")
             DispatchQueue.main.async {
                 // Update sessionUrl on main thread when the network call completes
-                self.sessionUrl = result
+                self.sessionResult = result
             }
         }
     }
