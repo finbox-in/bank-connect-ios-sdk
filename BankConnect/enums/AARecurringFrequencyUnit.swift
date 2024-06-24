fb6367209f93c1149eb4edd881cd80ecc388f4fe
Srikar Reddy <gsrikar2010@gmail.com>
2024-06-24 21:04:35 +0530

    BANC-2537 Add AA Recurring Frequency Unit

--- BankConnect/enums/AARecurringFrequencyUnit.swift
+++ BankConnect/enums/AARecurringFrequencyUnit.swift
@@ -5,4 +5,18 @@
 //  Created by Srikar on 24/06/24.
 //
 
-import Foundation
+/// Account Aggregator Recurring Pull Frequency Unit
+public enum AARecurringFrequencyUnit: String {
+    
+    /// Fetch AA hourly
+    case HOUR = "hour"
+    
+    /// Fetch AA daily
+    case DAY = "day"
+    
+    /// Fetch AA monthly
+    case MONTH = "month"
+    
+    /// Fetch AA yearly
+    case YEAR = "year"
+}
--- BankConnect/enums/JourneyMode.swift
+++ BankConnect/enums/JourneyMode.swift
@@ -5,8 +5,6 @@
 //  Created by Ashutosh Jena on 12/12/23.
 //
 
-import Foundation
-
 /// Specifies the mode in which the journey should be initiated.
 public enum JourneyMode: String {
     case NORMAL = ""
