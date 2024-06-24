9ec41e0c9ddc37b157a237f6a501a527aae6a531
Srikar Reddy <gsrikar2010@gmail.com>
2024-06-24 21:05:14 +0530

    BANC-2537 Add AA Journey Mode

--- BankConnect/enums/AAJourneyMode.swift
+++ BankConnect/enums/AAJourneyMode.swift
@@ -5,4 +5,12 @@
 //  Created by Srikar on 24/06/24.
 //
 
-import Foundation
+/// Account Aggregator Journey Mode
+public enum AAJourneyMode: String {
+    
+    /// Fetch once and Recurring from second time
+    case ONCE_WITH_RECURRING = "once_with_recurring"
+    
+    /// Only Recurring fetches
+    case ONLY_RECURRING = "only_recurring"
+}
