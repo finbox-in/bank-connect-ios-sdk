11cb5ad116d2fdfcd522313cc575310de173525c
Srikar Reddy <gsrikar2010@gmail.com>
2024-08-29 14:16:51 +0530

    BANC-3036 Use Host function in newer versions and host parameter otherwise

--- BankConnect/web/WebViewCoordinator.swift
+++ BankConnect/web/WebViewCoordinator.swift
@@ -11,9 +11,18 @@ class WebViewCoordinator: NSObject, WKNavigationDelegate {
         self.parent = parent
     }
     
+    func getHost(url: URL) -> Bool {
+        let host = if #available(iOS 16.0, *) {
+            url.host()
+        } else {
+            url.host
+        }
+        return host?.contains("finbox.in") == false
+    }
+    
     func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
         if let url = navigationAction.request.url {
-            if url.scheme == "mailto" || url.scheme == "tel" || url.host()?.contains("finbox.in") == false {
+            if url.scheme == "mailto" || url.scheme == "tel" || getHost(url: url) {
                 if UIApplication.shared.canOpenURL(url) {
                     UIApplication.shared.open(url, options: [:], completionHandler: nil)
                     decisionHandler(.cancel)
